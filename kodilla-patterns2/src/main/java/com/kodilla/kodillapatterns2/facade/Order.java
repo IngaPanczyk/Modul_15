fef4cfbe0416309aaa942c4b7de3ff677b5f38d2
IngaPanczyk <ingawaaa@gmail.com>
2020-12-18 23:10:55 +0100

    wzorzec fasada

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/Order.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/Order.java
@@ -1,4 +1,73 @@
 package com.kodilla.kodillapatterns2.facade;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private ProductService productService;
+    private final List<Item> items = new ArrayList<>();
+    private final Long orderId;
+    private final Long userId;
+    private boolean idPaid = false;
+    private boolean isVerified = false;
+    private boolean isSubmitted = false;
+
+    public Order( Long orderId, Long userId, ProductService productService) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.productService = productService;
+    }
+    public BigDecimal calculeteValue() {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Item item : items) {
+            sum = sum.add(productService.getPrice(item.getProductId())
+                    .multiply(new BigDecimal(item.getQty())));
+        }
+        return sum;
+    }
+
+    public ProductService getProductService() {
+        return productService;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public boolean isIdPaid() {
+        return idPaid;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public boolean isSubmitted() {
+        return isSubmitted;
+    }
+
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void setIdPaid(boolean idPaid) {
+        this.idPaid = idPaid;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
+
+    public void setSubmitted(boolean submitted) {
+        isSubmitted = submitted;
+    }
 }
